728c6f43457a75e74d4bf4fcea0d9a3a61a784d4
Koh Chun Wei <tp067580@mail.apu.edu.my>
2025-07-13 22:38:10 +0800

    Fix Bug Completed

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ try{
 if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.get("*", (req, res) => {
+    app.get("/*wildcard", (req, res) => {
         res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
     });
 }
